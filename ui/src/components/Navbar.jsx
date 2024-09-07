2ce7bd3a0f34e5136355e5c87247ebc621d1affa
AMARNATHKV <amarnathramanthali@gmail.com>
2024-09-07 11:13:43 +0530

    updated

--- ui/src/components/Navbar.jsx
+++ ui/src/components/Navbar.jsx
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="w-full bg-gradient-to-r from-blue-500 via-blue-400 to-purple-500 shadow-md p-4">
-      <div className="container mx-auto flex justify-end items-center">
-        {/* Menu items */}
+      <div className="container mx-auto flex justify-between items-center">
+       
+        <h1 className="text-2xl font-bold text-white">CertiHub</h1>
+
         <div className="flex space-x-2">
           <Link to="/">
             <button className="bg-white text-gray-800 font-bold py-2 px-4 rounded-full hover:bg-gray-100 transition duration-300 shadow-md">
